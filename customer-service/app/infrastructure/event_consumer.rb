25b50a297e59d2c1350238033f8755f2b98d3df4
Angel Morales <angel.morales@blautech.com>
2025-09-11 16:31:52 -0500

    config dead letter

--- customer-service/app/infrastructure/event_consumer.rb
+++ customer-service/app/infrastructure/event_consumer.rb
@@ -2,7 +2,17 @@
 
 class EventConsumer
   include Sneakers::Worker
-  from_queue "orders", ack: true, durable: true
+  from_queue "orders_created",
+             ack: true,
+             durable: true,
+             exchange: "orders_exchange",
+             exchange_type: :topic,
+             routing_key: "order.created",
+             arguments: {
+               "x-dead-letter-exchange" => "DLX_orders_created_exchange",
+               "x-dead-letter-routing-key" => "order.created"
+             }
+
   def work(message_payload)
     data = JSON.parse(message_payload)
     Rails.logger.info "Received message: #{data}"
@@ -10,8 +20,7 @@ class EventConsumer
     ack!  # confirmamos que fue procesado
   rescue StandardError => e
     Rails.logger.error "Error processing message: #{e.message}"
-    # Optionally, reject the message to requeue it or send it to a dead-letter queue
-    reject!(:requeue => false)
+    reject! # send it to a dead-letter queue
   end
   def update_customer_order_count(customer_id)
     Customer.where(customer_id: customer_id).update_all("orders_count = orders_count + 1")
