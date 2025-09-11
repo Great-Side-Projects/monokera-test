5c2cc2d9ab7dc080069fc9ecbf61edb87d80914b
Angel Morales <angel.morales@blautech.com>
2025-09-11 12:49:51 -0500

    update_customer_order_count

--- customer-service/app/infrastructure/event_consumer.rb
+++ customer-service/app/infrastructure/event_consumer.rb
@@ -6,10 +6,14 @@ class EventConsumer
   def work(message_payload)
     data = JSON.parse(message_payload)
     Rails.logger.info "Received message: #{data}"
+    update_customer_order_count(data["customer_id"])
     ack!  # confirmamos que fue procesado
   rescue StandardError => e
     Rails.logger.error "Error processing message: #{e.message}"
     # Optionally, reject the message to requeue it or send it to a dead-letter queue
     reject!(:requeue => false)
+  end
+  def update_customer_order_count(customer_id)
+    Customer.where(customer_id: customer_id).update_all("orders_count = orders_count + 1")
   end
 end
